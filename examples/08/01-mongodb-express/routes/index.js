58827bca82a662b011f8ec655113f0e87af52157
Mikael Olsson <mikael.olsson@emmio.se>
2019-12-05 13:25:16 +0100

    Example 08-01: Added edit user handling

--- examples/08/01-mongodb-express/routes/index.js
+++ examples/08/01-mongodb-express/routes/index.js
@@ -41,9 +41,52 @@ router.get('/helloworld', function (req, res) {
   res.render('helloworld', { title: 'Hello, World!' });
 });
 
+/* GET edit user page. */
+router.get('/edituser/:userid', function (req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+
+  collection.find({ _id: req.params.userid }, {}, function (e, data) {
+    res.render('edituser', {
+      userdata: data
+    });
+  });
+});
+
 /* GET New User page. */
 router.get('/newuser', function (req, res) {
   res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST Update user. */
+router.post('/updateuser', function (req, res) {
+  // Set our internal DB variable
+  var db = req.db;
+
+  // Get our form values. These rely on the "name" attributes
+  let id = req.body.id;
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  // Set our collection
+  var collection = db.get('usercollection');
+
+  // Submit to the DB
+  collection.update({'_id': id}, {
+    $set: {
+      'username': userName,
+      'email': userEmail
+    }
+  }, function (err, doc) {
+    if (err) {
+      // If it failed, return error
+      res.send("There was a problem updating the information in the database.");
+    }
+    else {
+      // And forward to success page
+      res.redirect("userlist");
+    }
+  });
 });
 
 /* GET Userlist page. */
