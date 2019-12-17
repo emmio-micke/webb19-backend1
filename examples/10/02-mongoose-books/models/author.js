b9fb76fe466d4f659a8cc65c3ee172186dd1e904
Mikael Olsson <mikael.olsson@emmio.se>
2019-12-17 11:22:34 +0100

    Example 10-02: Mongoose

---
+++ examples/10/02-mongoose-books/models/author.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+    {
+        first_name: { type: String, required: true, max: 100 },
+        family_name: { type: String, required: true, max: 100 },
+        date_of_birth: { type: Date },
+        date_of_death: { type: Date },
+    }
+);
+
+// Virtual for author's full name
+AuthorSchema
+    .virtual('name')
+    .get(function () {
+
+        // To avoid errors in cases where an author does not have either a family name or first name
+        // We want to make sure we handle the exception by returning an empty string for that case
+
+        var fullname = '';
+        if (this.first_name && this.family_name) {
+            fullname = this.family_name + ', ' + this.first_name
+        }
+        if (!this.first_name || !this.family_name) {
+            fullname = '';
+        }
+
+        return fullname;
+    });
+
+// Virtual for author's lifespan
+AuthorSchema
+    .virtual('lifespan')
+    .get(function () {
+        return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+    });
+
+// Virtual for author's URL
+AuthorSchema
+    .virtual('url')
+    .get(function () {
+        return '/catalog/author/' + this._id;
+    });
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
